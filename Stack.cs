7a64c7bc416ff79651213522eb9f13c890d3b779
Pawat <pawat@Pawats-MacBook-Pro.local>
2018-10-30 14:08:44 +0700

    add stack

---
+++ Stack.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace stack{
+  public class Stack{
+    private List<Object> _list = new List<Object>();
+    public void Push(Object obj){
+      if(obj == null)
+        throw new InvalidOperationException("no object found");
+      _list.Add(obj);
+    }
+
+    public Object Pop(){
+      if(_list.Count == 0)
+        throw new InvalidOperationException("not found Item");
+
+      var lastIndex = _list.Count - 1;
+      var TopReturn = _list[lastIndex];
+
+      _list.RemoveAt(lastIndex);
+
+      return TopReturn;
+    }
+
+    public void Clear(){
+      _list.Clear()
+    }
+  }
+}
